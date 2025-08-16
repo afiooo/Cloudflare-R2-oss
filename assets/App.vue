a91e5e6104fe642a2fd83f2fe58676c412e95348
afiooo <149303909+afiooo@users.noreply.github.com>
2025-08-16 13:43:16 +0800

    Update App.vue

--- assets/App.vue
+++ assets/App.vue
@@ -54,12 +54,11 @@
           @contextmenu.prevent
         >
           <div class="file-icon">
-            <img
-              src="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/4.0.0/png/file/folder/materialicons/36dp/2x/baseline_folder_black_36dp.png"
-              width="36"
-              height="36"
-              alt="Folder"
-            />
+            <!-- ========== 修改点 1：将外部图片替换为内联 SVG ========== -->
+            <svg xmlns="http://www.w3.org/2000/svg" height="36" viewBox="0 0 24 24" width="36" fill="#5f6368">
+              <path d="M0 0h24v24H0V0z" fill="none"/>
+              <path d="M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z"/>
+            </svg>
           </div>
           <span class="file-name">..</span>
         </div>
@@ -75,12 +74,11 @@
           "
         >
           <div class="file-icon">
-            <img
-              src="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/4.0.0/png/file/folder/materialicons/36dp/2x/baseline_folder_black_36dp.png"
-              width="36"
-              height="36"
-              alt="Folder"
-            />
+            <!-- ========== 修改点 2：将外部图片替换为内联 SVG ========== -->
+            <svg xmlns="http://www.w3.org/2000/svg" height="36" viewBox="0 0 24 24" width="36" fill="#5f6368">
+              <path d="M0 0h24v24H0V0z" fill="none"/>
+              <path d="M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z"/>
+            </svg>
           </div>
           <span
             class="file-name"
